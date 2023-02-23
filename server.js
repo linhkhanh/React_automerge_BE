8a3d568378b9a468e1eacc4ef37fcd2e7d9eb585
linhkhanh <khanhlinh220592@gmail.com>
2023-02-23 07:56:44 +0700

    update server

--- server.js
+++ server.js
@@ -1,50 +1,54 @@
-const express = require('express')
-const path = require('path')
-const fs = require('fs')
-const cors = require('cors')
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-let app = express()
+let app = express();
 var options = {
-	inflate: true,
-	limit: '100kb',
-	type: 'application/octet-stream'
+  inflate: true,
+  limit: "100kb",
+  type: "application/octet-stream",
 };
 app.use(bodyParser.raw(options));
 
-try { 
-	fs.mkdirSync(path.join(__dirname, 'data'))
+try {
+  fs.mkdirSync(path.join(__dirname, "data"));
 } catch (err) {
-	if (err.code !== 'EEXIST') {
-		console.error(err)
-	} 
+  if (err.code !== "EEXIST") {
+    console.error(err);
+  }
 }
 
+let corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"]
+};
 
-app.use(cors())
+app.options('/:id', cors(corsOptions));
+app.get("/:id", cors(corsOptions), (req, res) => {
+  let id = req.params.id;
+  let filename = path.join(__dirname, "data", id);
+  fs.stat(filename, (err, stats) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send("Not found");
+    } else {
+      res.sendFile(filename);
+      console.log("sending");
+    }
+  });
+});
 
-app.get('/:id', (req, res) => {
-	let id = req.params.id
-	let filename = path.join(__dirname, 'data', id)
-	fs.stat(filename, (err, stats) => {
-		if (err) {
-			console.error(err)
-			res.status(404).send('Not found')
-		} else { 
-			res.sendFile(filename)
-			console.log('sending')
-		}
-	})
-})
+app.post("/:id", cors(corsOptions), (req, res) => {
+  let id = req.params.id;
+  fs.writeFileSync(path.join(__dirname, "data", id), req.body);
+  res.status(200).send("ok");
+});
 
-app.post('/:id', (req, res) => {
-	let id = req.params.id
-	fs.writeFileSync(path.join(__dirname, 'data', id), req.body)
-	res.status(200).send('ok')
-})
-
-const port = 5000
+const port = 5000;
 
 app.listen(5000, () => {
-	console.log('listening on http://localhost:' + port)
-})
+  console.log("listening on http://localhost:" + port);
+});
